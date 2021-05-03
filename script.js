d2cce9c11bf58a35bd911d326c1cb24ca012e19c
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2021-05-03 16:41:15 -0700

    fixed some codes to color rate

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const API_KEY = 'api_key=3754416f7cb9e5010ce62384e4bf33c3';
 const BASE_URL ='https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL ='https://image.tmdb.org/t/p/w500';
+const main = document.getElementById('main');
 
 getMovies(API_URL);
 
@@ -11,11 +12,12 @@ function getMovies(url) {
 
     fetch(url).then(res => res.json()).then(data => {
         console.log(data.results)
-        showMovies(data);
+        showMovies(data.results);
     })
 }
 
 function showMovies(data) {
+    main.innerHTML ='';
 
     data.forEach(movie => {
         const {title, poster_path, vote_average, overview} = movie;
@@ -32,6 +34,7 @@ function showMovies(data) {
             ${overview}
         </div>
         `
+        main.appendChild(movieEl);
     })
 }
 
