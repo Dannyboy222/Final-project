c7baf055e7c772c4f2d6026e39573d076d3210aa
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2021-05-03 17:38:51 -0700

    changed some names

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const API_KEY = 'api_key=3754416f7cb9e5010ce62384e4bf33c3';
 const BASE_URL ='https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const API_URL = BASE_URL + '/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&'+API_KEY;
 const IMG_URL ='https://image.tmdb.org/t/p/w500';
 const main = document.getElementById('main');
 
